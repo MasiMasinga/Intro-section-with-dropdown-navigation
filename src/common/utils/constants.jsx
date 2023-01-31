667ba2b6f6e821dce9b989f4550043766c150d62
Masi <51009658+MasiMasinga@users.noreply.github.com>
2023-01-31 21:48:36 +0200

    feat - add constant colors

--- src/common/utils/constants.jsx
+++ src/common/utils/constants.jsx
@@ -1,5 +1,9 @@
 export const Colors = {
     black: "#000000",
     white: "#FFFFFF",
-    mediumGrey: "#696969"
+    mediumGrey: "#696969",
+    yellow: "#FFD700",
+    purple: "#800080",
+    skyBlue: "#87CEEB",
+    blue: "#0000FF",
 }
